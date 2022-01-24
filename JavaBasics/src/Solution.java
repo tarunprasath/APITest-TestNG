c0af2d3cc00fae84dcf569917f1f6f376f65e632
arunprasad18@gmail.com <Arun Prasath>
2022-01-24 23:58:16 +0800

    Added the Solution.java file of JavaBasics

---
+++ JavaBasics/src/Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+
+class Solution {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("Hello, World!");
+        strings.add("Welcome to CoderPad.");
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
